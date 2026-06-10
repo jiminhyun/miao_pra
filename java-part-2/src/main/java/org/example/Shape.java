a77dd4b5970c30868ef661723322bfedc81cbc34
jiminhyun <hgm1171@naver.com>
2026-06-10 14:25:01 +0900

    problem

---
+++ java-part-2/src/main/java/org/example/Shape.java
@@ -0,0 +1,17 @@
+package org.example;
+
+class Shape {
+    private String color = "black";
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void draw() {
+        System.out.println("Color is " + color);
+    }
+}
